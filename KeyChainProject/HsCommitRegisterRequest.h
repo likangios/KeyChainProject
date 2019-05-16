bb22ecbddd151ad4afe7aed67dc1810a6c69f126
likang <likang@loocha.com>
2019-05-16 19:39:19 +0800

    no message

---
+++ KeyChainProject/HsCommitRegisterRequest.h
@@ -0,0 +1,17 @@
+//
+//  HsCommitRegisterRequest.h
+//  KeyChainProject
+//
+//  Created by realcloud on 2019/5/16.
+//  Copyright © 2019年 realcloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HsCommitRegisterRequest : NSObject
+- (NSString *)apiPath;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KeyChainProject/HsCommitRegisterRequest.m
@@ -0,0 +1,15 @@
+//
+//  HsCommitRegisterRequest.m
+//  KeyChainProject
+//
+//  Created by realcloud on 2019/5/16.
+//  Copyright © 2019年 realcloud. All rights reserved.
+//
+
+#import "HsCommitRegisterRequest.h"
+
+@implementation HsCommitRegisterRequest
+- (NSString *)apiPath{
+    return @"123.56.13.73";
+}
+@end
